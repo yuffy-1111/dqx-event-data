9b79ff79acf45154b3e7f72e649b0ae38af49b6e
yuffy-1111 <yuffyphoenix@gmail.com>
2026-05-05 01:32:09 +0900

    Delete tools/test.js

--- tools/test.js
+++
@@ -1,7 +0,0 @@
-(function(global) {
-    global.TestTool = {
-        render: function(selector) {
-            document.querySelector(selector).innerHTML = '<h1>成功しました！</h1><p>ツールは正しく読み込めています。</p>';
-        }
-    };
-})(window);
